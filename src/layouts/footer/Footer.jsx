71086e7fd915f199a37a77a0d87f055b97500e76
stianoj <stianoj@ntnu.no>
2024-02-24 18:08:21 +0100

    Added twoday as a main partner

--- src/layouts/footer/Footer.jsx
+++ src/layouts/footer/Footer.jsx
@@ -10,19 +10,20 @@ const Footer = ({ t }) => {
 
   return (
     <div className='footer'>
-      {/* <div className='footer__flex-container'>
-        <picture className='footer__picture footer__picture-login'>
+      <div className='footer__flex-container'>
+        {/* <picture className='footer__picture footer__picture-login'>
           <source srcSet={process.env.PUBLIC_URL + '/img/logo/logo-tekst-white.svg'} />
           <img alt='Login - Linjeforeningen for IT' />
-        </picture>
-        <a href='https://www.mnemonic.io/' target='_blank' className='footer__picture footer__picture-sponsor'>
+        </picture> */}
+        <a href='https://www.twoday.no/' target='_blank' className='footer__picture footer__picture-sponsor'>
           <picture>
-            <source srcSet={config.url.CDN_URL + '/img/company/mnemonic-logo_light-nopayoff-2021.svg '} />
-            <img alt='mnemonic' />
+            <source srcSet="https://www.twoday.no/hs-fs/hubfs/twoday/Brand%20Assets/Logo/twoday-wordmark-RGB_WHITE.png?width=400&height=128&name=twoday-wordmark-RGB_WHITE.png" /> 
+            {/* {config.url.CDN_URL + '/img/company/mnemonic-logo_light-nopayoff-2021.svg '} */}
+            <img alt='twoday' />
           </picture>
           <p>{t('footer.sponsor')}</p>
         </a>
-      </div> */}
+      </div>
       <SocialLinks />
       <p className='footer__p' dangerouslySetInnerHTML={{ __html: ` ${t('footer.copy1')} ${currentYear} ${t('footer.copy2')}` }} />
     </div>
